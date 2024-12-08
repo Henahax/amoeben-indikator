fc219d92bfff3e9362291860d3393c43a9d83671
Henahax <mail@henahax.de>
2024-12-08 01:28:54 +0100

    db reset

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
-import db from '@astrojs/db';
-
 import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), db()],
+  integrations: [tailwind()],
 
   adapter: node({
     mode: 'standalone'
