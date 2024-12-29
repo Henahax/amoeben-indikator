6eaab905d90339df79befae4a855118836fbf8d4
Henahax <mail@henahax.de>
2024-12-29 03:54:45 +0100

    layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,21 +10,19 @@
 
 <app class="flex min-h-dvh flex-col">
 	<ModeWatcher />
-	<header class="flex justify-end">
+	<header class="flex justify-between">
+		<Button size="sm" variant="ghost" href="https://github.com/Henahax/amoeben-indikator">
+			<span class="flex items-center gap-2 text-xs opacity-75">
+				<i class="fa-brands fa-github"></i>
+				Sourcecode
+			</span>
+		</Button>
 		<Button on:click={toggleMode} variant="outline" size="icon">
 			<i class="fa-solid fa-sun hidden dark:block"></i>
 			<i class="fa-solid fa-moon block dark:hidden"></i>
 		</Button>
 	</header>
-	<main class="mx-auto grow content-center p-8 max-w-screen-2xl w-full">
+	<main class="mx-auto grow content-center p-4 max-w-screen-2xl w-full">
 		{@render children()}
 	</main>
-	<footer class="flex justify-end text-xs">
-		<Button size="sm" variant="ghost" href="https://github.com/Henahax/amoeben-indikator">
-			<span class="flex items-center gap-2 text-xs">
-				<i class="fa-brands fa-github"></i>
-				Sourcecode
-			</span>
-		</Button>
-	</footer>
 </app>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,23 @@
 	import { Progress } from '$lib/components/ui/progress';
 	import { scorer } from '$lib/state/score.svelte.js';
 
+	import { Tween } from 'svelte/motion';
+	import { cubicOut } from 'svelte/easing';
+
 	let { data } = $props();
 	const { entries, scale } = data;
 
 	scorer.score = Number(getScore(data.entries));
+
+	let progress = $state(
+		new Tween(0, {
+			delay: 0,
+			duration: 2500,
+			easing: cubicOut
+		})
+	);
+
+	progress.target = scorer.score * 100;
 
 	function getScore(stuff: any) {
 		if (stuff.length === 0) {
@@ -40,7 +53,7 @@
 				<li
 					title={scaleItem.name + ', ≥ ' + Number(scaleItem.value).toString()}
 					class="flex flex-col items-center {isActive(index)
-						? 'animate-pulse font-bold'
+						? 'text-primary animate-pulse font-bold'
 						: 'opacity-25'}"
 				>
 					<i class="{scaleItem.icon} {isActive(index) ? 'text-3xl' : ''}"></i>
@@ -48,36 +61,41 @@
 				</li>
 			{/each}
 		</ul>
-		<Progress value={scorer.score * 100} title={"aktueller Wert: " + scorer.score.toString()} />
+		<Progress value={progress.current} title={'aktueller Wert: ' + scorer.score} />
 	</section>
 
 	<section class="flex flex-col gap-8">
-		<div class="mx-auto grid w-fit grid-cols-[auto_1fr] gap-16 sm:gap-8">
+		<ul class="divide-y divide-neutral-500 divide-opacity-25">
 			{#each data.entries as entry}
-				<div class="grid grid-cols-1 sm:grid-cols-2 content-center gap-y-2 gap-x-8">
-					<div class="flex flex-col justify-center">
-						<div>{entry.userName}</div>
-						<div class="text-xs text-neutral-500">
-							{new Date(entry.date).toLocaleString('de-DE', {
-								day: '2-digit',
-								month: '2-digit',
-								year: 'numeric',
-								hour: '2-digit',
-								minute: '2-digit',
-								second: '2-digit'
-							})}
+				<li class="grid grid-cols-[auto_auto_1fr] gap-x-16 gap-y-8 py-4">
+					<div class="grid-subgrid grid grid-cols-1 content-center gap-x-8 gap-y-2 sm:grid-cols-2 col-span-2 sm:col-span-1">
+						<div class="flex flex-col justify-center">
+							<div>{entry.userName}</div>
+							<div class="text-xs text-neutral-500">
+								{new Date(entry.date).toLocaleString('de-DE', {
+									day: '2-digit',
+									month: '2-digit',
+									year: 'numeric',
+									hour: '2-digit',
+									minute: '2-digit',
+									second: '2-digit'
+								})}
+							</div>
+						</div>
+						<div class="flex items-center gap-2 font-bold">
+							<i class="{entry.scaleIcon} text-xl"></i>
+							{entry.scaleName}
 						</div>
 					</div>
-					<div class="flex items-center gap-2 font-bold">
-						<i class="{entry.scaleIcon} text-xl"></i>
-						{entry.scaleName}
+
+					<div class="flex items-center text-sm">
+						{entry.description}
 					</div>
-				</div>
-				<div class="flex items-center">
-					{entry.description}
-				</div>
+				</li>
 			{/each}
-		</div>
-		<Button class="mx-auto w-fit" href="/new">Neuer Eintrag</Button>
+		</ul>
 	</section>
+</div>
+<div class="sticky bottom-0 w-full flex justify-end py-4">
+	<Button class="w-fit" href="/new">Neuer Eintrag</Button>
 </div>
