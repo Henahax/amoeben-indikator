58b505f7b297af20948801bdee4cf1d285156c0c
Henahax <mail@henahax.de>
2025-01-04 00:07:45 +0100

    todo: entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-# Debugging: Print the current directory and list files
-echo "Current directory: $(pwd)"
-ls -l
-
-# Debugging: Print the content of the entrypoint script
-echo "Content of entrypoint.sh:"
-cat /app/entrypoint.sh
-
 # Wait for PostgreSQL to be ready
 until pg_isready -h db -p 5432 -U postgres; do
   echo "Waiting for PostgreSQL to be ready..."
