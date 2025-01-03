4d503a8de4449af353fb25f3b1ebc019fb226912
Henahax <mail@henahax.de>
2025-01-04 00:02:49 +0100

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,10 @@
 echo "Current directory: $(pwd)"
 ls -l
 
+# Debugging: Print the content of the entrypoint script
+echo "Content of entrypoint.sh:"
+cat /app/entrypoint.sh
+
 # Wait for PostgreSQL to be ready
 until pg_isready -h db -p 5432 -U postgres; do
   echo "Waiting for PostgreSQL to be ready..."
