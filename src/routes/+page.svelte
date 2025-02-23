34a4136b46a01a42c1b8821a24345fcea9e773e7
Henahax <mail@henahax.de>
2025-02-23 22:22:00 +0100

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
 		let highest: { id: string; value: number } | undefined;
 
 		data.scales.forEach((scale: any) => {
-			if (data.score >= scale.value - 0.1) {
+			if (data.score >= scale.value) {
 				highest = scale;
 			}
 		});
@@ -44,21 +44,18 @@
 		<div class="flex w-full justify-between">
 			{#each data.scales as scale}
 				<div
-					class="grid grid-cols-1 place-items-center text-3xl {scale.id === highestReached
-						? 'text-green-500'
-						: 'opacity-25'}"
+					class={`grid grid-cols-1 place-items-center text-3xl ${scale.id === highestReached ? 'highest-reached' : 'opacity-25'}`}
 					title={scale.name}
 				>
-					<i class={scale.icon}></i>
-					<span class="hidden text-lg font-bold sm:block">{scale.name}</span>
+					<i class={`${scale.icon} ${scale.id === highestReached ? 'animate-pulse' : ''}`}></i>
+					<span class="hidden text-base font-bold sm:block">{scale.name}</span>
 				</div>
 			{/each}
 		</div>
 		<meter
 			id="indicator"
-			class="h-8 w-full"
-			low="0.25"
-			high="0.75"
+			low="0.4"
+			high="0.8"
 			optimum="1"
 			value={data.score}
 			title={data.score.toString()}
@@ -66,27 +63,31 @@
 		</meter>
 	</section>
 
-	<section class="flex flex-col items-center gap-8 p-4">
+	<section class="flex w-full flex-col items-center gap-8">
 		<a href="/new" class="button button-primary">Neuer Eintrag</a>
 
-		<div class="grid grid-cols-[auto_auto_1fr] items-center gap-8">
-			{#each data.entries as entry}
-				<div class="flex flex-col items-center justify-center">
-					<span>{entry.users?.username}</span>
-					<div class="inline-flex flex-wrap justify-center gap-x-2">
-						<span>{formatDate(entry.entries.timestamp ?? '')}</span>
-						<span>{formatTime(entry.entries.timestamp ?? '')}</span>
-					</div>
-				</div>
-				<div class="flex flex-col items-center justify-center text-2xl">
-					<i class={data.scales.find((scale) => scale.id === entry.entries.scaleId)?.icon}></i>
-					<span class="text-base"
-						>{data.scales.find((scale) => scale.id === entry.entries.scaleId)?.name}</span
-					>
-				</div>
+		<div class="card w-fit px-6 py-2">
+			<div class="grid grid-cols-[auto_auto_1fr] items-center divide-y divide-neutral-500">
+				{#each data.entries as entry}
+					<div class="col-span-full grid grid-cols-subgrid items-center gap-4 py-4 sm:gap-8">
+						<div class="flex flex-col">
+							<span>{entry.users?.username}</span>
+							<div class="text-xs text-neutral-500">
+								<span>{formatDate(entry.entries.timestamp ?? '')}</span>
+								<span>{formatTime(entry.entries.timestamp ?? '')}</span>
+							</div>
+						</div>
+						<div class="flex flex-col items-center text-lg">
+							<i class={data.scales.find((scale) => scale.id === entry.entries.scaleId)?.icon}></i>
+							<span class="text-base"
+								>{data.scales.find((scale) => scale.id === entry.entries.scaleId)?.name}</span
+							>
+						</div>
 
-				<div>{entry.entries.comment}</div>
-			{/each}
+						<div>{entry.entries.comment}</div>
+					</div>
+				{/each}
+			</div>
 		</div>
 	</section>
 </div>
@@ -94,5 +95,31 @@
 <style>
 	section {
 		width: 100%;
+	}
+
+	.highest-reached {
+		color: var(--color-business-indigo-500);
+	}
+
+	meter {
+		height: 2rem;
+		width: 100%;
+		opacity: 0.75;
+	}
+
+	meter::-webkit-meter-bar {
+		background: var(--color-neutral-800);
+	}
+
+	meter::-webkit-meter-optimum-value {
+		background: green;
+	}
+
+	meter::-webkit-meter-suboptimum-value {
+		background: yellow;
+	}
+
+	meter::-webkit-meter-even-less-good-value {
+		background: red;
 	}
 </style>
--- src/routes/new/+page.server.ts
+++ src/routes/new/+page.server.ts
@@ -1,5 +1,7 @@
 import { db } from '$lib/server/db';
 import { scales, users } from '$lib/server/db/schema';
+import { fail, redirect } from '@sveltejs/kit';
+import * as argon2 from '@node-rs/argon2';
 
 let myScales = await db.select().from(scales).orderBy(scales.value);
 let myUsers = await db.select().from(users);
@@ -10,3 +12,46 @@ export const load = async () => {
         users: myUsers
     };
 };
+
+export const actions = {
+    default: async ({ request }) => {
+
+        let data = await request.formData();
+
+        let userId = Number(data.get("user"));
+        let scaleId = Number(data.get("scale"));
+        let description = data.get("description")?.toString();
+        let password = data.get("password")?.toString();
+
+        console.log({ userId, scaleId, description, password });
+
+        if (userId < 1) {
+            return fail(400, { userId, userInvalid: true, scaleId, description });
+        }
+
+        if (scaleId < 1) {
+            return fail(400, { scaleId, scaleInvalid: true, userId, description });
+        }
+
+        if (!description || description.length < 1) {
+            return fail(400, { description, descriptionInvalid: true, userId, scaleId });
+        }
+
+        if (!password || password.length < 1) {
+            return fail(400, { password, passwordInvalid: true, userId, scaleId, description });
+        }
+
+        const user = myUsers.find(user => Number(user.id) === userId);
+
+        if (!user || !(await argon2.verify(user.passwordHash, password))) {
+            return fail(400, { password, passwordInvalid: true, userId, scaleId, description });
+        }
+
+        let entry: any = { "user_id": userId, "scale_id": scaleId, "date": new Date().toISOString(), "description": description };
+
+
+        await db.insert(entry).values(entry);
+
+        redirect(303, "/");
+    }
+}
