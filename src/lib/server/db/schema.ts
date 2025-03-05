cada721163266a373e33d4fc30a4b9cfb0c9b630
Henahax <mail@henahax.de>
2025-03-05 21:40:16 +0100

    rework init

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,32 +1,20 @@
-import { sql } from "drizzle-orm";
-import { sqliteTable, int, real, text } from 'drizzle-orm/sqlite-core';
+import { pgTable, serial, text, integer, timestamp } from 'drizzle-orm/pg-core';
 
-export const users = sqliteTable('users', {
-	id: int('id').primaryKey(),
+export const user = pgTable('user', {
+	id: text('id').primaryKey(),
+	age: integer('age'),
 	username: text('username').notNull().unique(),
 	passwordHash: text('password_hash').notNull()
 });
 
-export const scales = sqliteTable('scales', {
-	id: int('id').primaryKey(),
-	name: text('name').notNull().unique(),
-	value: real('value').unique(),
-	icon: text('icon').notNull()
+export const session = pgTable('session', {
+	id: text('id').primaryKey(),
+	userId: text('user_id')
+		.notNull()
+		.references(() => user.id),
+	expiresAt: timestamp('expires_at', { withTimezone: true, mode: 'date' }).notNull()
 });
 
-export const entries = sqliteTable('entries', {
-	id: int('id').primaryKey(),
-	userId: int('user_id').references(() => users.id),
-	scaleId: int('scale_id').references(() => scales.id),
-	timestamp: text('timestamp').default(sql`(CURRENT_TIMESTAMP)`),
-	comment: text('comment').notNull()
-})
+export type Session = typeof session.$inferSelect;
 
-export type EntriesSelect = typeof entries.$inferSelect
-export type EntriesInsert = typeof entries.$inferInsert
-
-export type ScalesSelect = typeof scales.$inferSelect;
-export type ScalesInsert = typeof scales.$inferInsert;
-
-export type UsersSelect = typeof users.$inferSelect;
-export type UsersInsert = typeof users.$inferInsert;
+export type User = typeof user.$inferSelect;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,124 +1,59 @@
 <script lang="ts">
-	let { data } = $props();
-
-	let highestReached = $derived(getHighestReached(data));
-
-	function getHighestReached(data: any) {
-		let highest: { id: string; value: number } | undefined;
-
-		data.scales.forEach((scale: any) => {
-			if (data.score >= scale.value) {
-				highest = scale;
-			}
-		});
-
-		if (highest) {
-			return highest.id;
-		}
-
-		return data.scales[0].id;
-	}
-
-	function formatDate(dateString: string) {
-		const date = new Date(dateString);
-		return date.toLocaleString('de-DE', {
-			day: '2-digit',
-			month: '2-digit',
-			year: 'numeric'
-		});
-	}
-
-	function formatTime(dateString: string) {
-		const date = new Date(dateString);
-		return date.toLocaleString('de-DE', {
-			hour: '2-digit',
-			minute: '2-digit'
-		});
-	}
+	import Counter from './Counter.svelte';
+	import welcome from '$lib/images/svelte-welcome.webp';
+	import welcomeFallback from '$lib/images/svelte-welcome.png';
 </script>
 
-<div class="flex w-full flex-col items-center gap-16">
-	<section class="flex w-full flex-col items-center gap-8">
-		<h1 class="text-4xl">Amöben Indikator</h1>
-		<div class="flex w-full justify-between">
-			{#each data.scales as scale}
-				<div
-					class={`grid grid-cols-1 place-items-center text-3xl ${scale.id === highestReached ? 'highest-reached' : 'opacity-25'}`}
-					title={scale.name}
-				>
-					<i class={`${scale.icon} ${scale.id === highestReached ? 'animate-pulse' : ''}`}></i>
-					<span class="hidden text-base font-bold sm:block">{scale.name}</span>
-				</div>
-			{/each}
-		</div>
-		<meter
-			id="indicator"
-			low="0.4"
-			high="0.8"
-			optimum="1"
-			value={data.score}
-			title={data.score.toString()}
-		>
-		</meter>
-	</section>
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
 
-	<section class="flex w-full flex-col items-center gap-8">
-		<a href="/new" class="button button-primary">Neuer Eintrag</a>
+<section>
+	<h1>
+		<span class="welcome">
+			<picture>
+				<source srcset={welcome} type="image/webp" />
+				<img src={welcomeFallback} alt="Welcome" />
+			</picture>
+		</span>
 
-		<div class="card w-fit px-6 py-2">
-			<div class="grid grid-cols-[auto_auto_1fr] items-center divide-y divide-neutral-500">
-				{#each data.entries as entry}
-					<div class="col-span-full grid grid-cols-subgrid items-center gap-4 py-4 sm:gap-8">
-						<div class="flex flex-col">
-							<span>{entry.users?.username}</span>
-							<div class="text-xs text-neutral-500">
-								<span>{formatDate(entry.entries.timestamp ?? '')}</span>
-								<span>{formatTime(entry.entries.timestamp ?? '')}</span>
-							</div>
-						</div>
-						<div class="flex flex-col items-center text-lg">
-							<i class={data.scales.find((scale) => scale.id === entry.entries.scaleId)?.icon}></i>
-							<span class="text-base"
-								>{data.scales.find((scale) => scale.id === entry.entries.scaleId)?.name}</span
-							>
-						</div>
+		to your new<br />SvelteKit app
+	</h1>
 
-						<div>{entry.entries.comment}</div>
-					</div>
-				{/each}
-			</div>
-		</div>
-	</section>
-</div>
+	<h2>
+		try editing <strong>src/routes/+page.svelte</strong>
+	</h2>
+
+	<Counter />
+</section>
 
 <style>
 	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
 		width: 100%;
 	}
 
-	.highest-reached {
-		color: var(--color-business-indigo-500);
-	}
-
-	meter {
-		height: 2rem;
+	.welcome {
+		display: block;
+		position: relative;
 		width: 100%;
-		opacity: 0.75;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
 	}
 
-	meter::-webkit-meter-bar {
-		background: var(--color-neutral-800);
-	}
-
-	meter::-webkit-meter-optimum-value {
-		background: green;
-	}
-
-	meter::-webkit-meter-suboptimum-value {
-		background: yellow;
-	}
-
-	meter::-webkit-meter-even-less-good-value {
-		background: red;
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
 	}
 </style>
