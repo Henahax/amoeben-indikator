485783fb6bff11cd964ab65587d20e2fcb6ef5e3
Henahax <mail@henahax.de>
2025-03-17 01:44:49 +0100

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,23 +2,24 @@
 	import type { PageProps } from './$types';
 
 	interface PageData {
-		scales: { name: string }[];
+		scales: { name: string; icon: string }[];
 	}
 
 	let { data }: { data: PageData } = $props();
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-
-<section>
-	<div class="flex flex-col">
-		<div class="grid w-full grid-cols-6">
+<section class="flex flex-col gap-8 text-center">
+	<h1 class="text-5xl">Amöben-Indikator</h1>
+	<div class="flex flex-col gap-2">
+		<div class="grid w-full grid-cols-[auto_auto_auto_auto_auto_auto] justify-between">
 			{#each data.scales as item}
-				<p>{item.name}</p>
+				<div class="flex flex-col items-center text-4xl">
+					<i class={item.icon}></i>
+					<p class="text-xl max-sm:hidden">{item.name}</p>
+				</div>
 			{/each}
 		</div>
-		<progress class="w-full" />
+		<progress value="0.5" class="w-full"></progress>
 	</div>
 </section>
 <section></section>
