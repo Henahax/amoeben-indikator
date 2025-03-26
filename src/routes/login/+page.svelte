c134321828679a918af26d8c5581334fe1f1ea5c
Henahax <mail@henahax.de>
2025-03-26 22:41:32 +0100

    dialogs

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,19 @@
+<form class="card flex w-full max-w-sm flex-col gap-4">
+	<div class="flex flex-col">
+		<label for="username">Benutzer</label>
+		<input id="username" type="text" placeholder="Benutzername" />
+	</div>
+	<div class="flex flex-col">
+		<label for="password">Passwort</label>
+		<input id="password" type="password" placeholder="Passwort" />
+	</div>
+	<div class="flex flex-col justify-center">
+		<div class="flex gap-2">
+			<button class="btn btn-primary grow">Anmelden</button>
+			<a class="btn btn-secondary" href="/">Zurück</a>
+		</div>
+		<div class="flex justify-center">
+			<button class="btn btn-tertiary">Registrieren</button>
+		</div>
+	</div>
+</form>
