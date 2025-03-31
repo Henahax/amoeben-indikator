6528fc6e034a42f032158266da210cc4bb19bbf8
Henahax <mail@henahax.de>
2025-03-31 16:08:37 +0200

    test

--- src/lib/server/db/db.ts
+++ src/lib/server/db/db.ts
@@ -3,9 +3,10 @@ import postgres from 'postgres';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-const client = postgres(env.DATABASE_URL);
+if (!env.DATABASE_URL) {
+    throw new Error('DATABASE_URL is not set');
+}
 
-export const db = drizzle(client, {
-    schema
-});
+// Prevent connection during build
+const client = process.env.SKIP_DB === 'true' ? null : postgres(env.DATABASE_URL);
+export const db = client ? drizzle(client, { schema }) : null;
