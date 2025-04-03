577f66974ef3ddc33a8f4ea715c1df165fd25803
Henahax <mail@henahax.de>
2025-04-04 01:11:12 +0200

    test db

--- src/lib/server/db/db.ts
+++ src/lib/server/db/db.ts
@@ -3,8 +3,17 @@ import postgres from 'postgres';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
-const client = postgres(env.DATABASE_URL!);
+let client: postgres.Sql<{}> | null = null;
 
-export const db = drizzle(client, {
-    schema
-});
+async function initializeDb() {
+    if (!client) {
+        client = postgres(env.DATABASE_URL!, { max: 10 });
+        console.log('Testing database connection...');
+        await client`SELECT 1`; // Test query to ensure the connection is ready
+        console.log('Database connection is ready');
+    }
+}
+
+await initializeDb();
+
+export const db = drizzle(client!, { schema });
