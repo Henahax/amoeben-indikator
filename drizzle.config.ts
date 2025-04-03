7fea85477bfc93c87bc4952c92243d7a3320af3b
Henahax <mail@henahax.de>
2025-04-03 21:04:08 +0200

    reset

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
 export default defineConfig({
 	out: "./drizzle",
-	schema: "$lib/server/db/schema.ts",
+	schema: "./src/lib/server/db/schema.ts",
 	dialect: "postgresql",
 	dbCredentials: {
 		url: process.env.DATABASE_URL
